db84fb4b6aaa67859268ef4a6ae19dfdea176d52
link <zhyzetton@outlook.com>
2026-02-09 23:08:56 +0800

    first commit

---
+++ lua/core/basic.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+
+vim.opt.autoread = true
